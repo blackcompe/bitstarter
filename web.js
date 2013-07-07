b3794da108f7d0a375f492abedd8e371e245454e
Ryan Beckett <beckett.ryan@gmail.com>
2013-07-07 13:05:48 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var html = fs.readFileSync('index.html', {encoding: "utf-8"}, function(err, data) {
+	var html = fs.readFileSync('index.html', function(err, data) {
 		if(err) throw err;
 		console.log(data);
-	});
+	}).toString();
 	response.send(html);
 });
 
